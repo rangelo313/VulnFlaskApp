2e62a7f7a42969ca705420a26dd4dc7db161c675
rangelo313 <rac16c@my.fsu.edu>
2022-05-02 17:22:28 -0400

    OpenRedirect

--- app.py
+++ app.py
@@ -30,6 +30,28 @@ def user():
     else:
         return redirect(url_for("login"))
 
+
+@app.route("/login_and_redirect")
+def login_and_redirect():
+    username = request.args.get("username")
+    password = request.args.get("password")
+    url = request.args.get("url")
+    if username is None or password is None or url is None:
+        return (
+            jsonify(
+                {"error": "username, password, and url parameters have to be provided"}
+            ),
+            400,
+        )
+
+    query = "SELECT id, username, access_level FROM user WHERE username = ? AND password = ?"
+    result = query_db(query, (username, password), True)
+    if result is None:
+        # vulnerability: Open Redirect
+        return redirect(url)
+    session["user_info"] = (result[0], result[1], result[2])
+    return jsonify({"success": True})
+
 @app.route("/logout")
 def logout():
     session.pop("user", None) #REDIRECT?
